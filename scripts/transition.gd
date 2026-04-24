8f7940d57deca17bd8ff504a7395cce5e52b5b94
nullishew <155263075+nullishew@users.noreply.github.com>
2026-04-23 23:46:45 -0400

    smoother transition animation

--- scripts/transition.gd
+++ scripts/transition.gd
@@ -9,9 +9,17 @@ signal out_finished()
 
 
 func _ready() -> void:
-	anim_player.play("in")
-	await anim_player.animation_finished
+	anim_player.play("transition")
+	# await anim_player.animation_finished
+	# in_finished.emit()
+	# anim_player.play("out")
+	# await anim_player.animation_finished
+	# out_finished.emit()
+
+
+func _on_in_finished():
 	in_finished.emit()
-	anim_player.play("out")
-	await anim_player.animation_finished
+
+
+func _on_out_finished():
 	out_finished.emit()
