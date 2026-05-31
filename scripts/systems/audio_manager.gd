898f2f56c8b34ca98359c832d9916140df94c9dd
nullishew <155263075+nullishew@users.noreply.github.com>
2026-05-31 06:08:55 -0400

    resolved settings menu audio sliders affecting wrong audio bus + rename soundtrack audio bus to bgm

--- scripts/systems/audio_manager.gd
+++ scripts/systems/audio_manager.gd
@@ -1,22 +1,26 @@
 extends Node
 
+
 const SOUNDTRACK: AudioStream = preload("res://resources/audio/palindome_jam_soundtrack.wav")
 const THUD_SOUND: AudioStream = preload("res://resources/audio/thud.wav")
 const TRANSITION_SOUND: AudioStream = preload("res://resources/audio/transition.wav")
 const TOWEL_DISPENSER_SOUND: AudioStream = preload("res://resources/audio/towel_dispenser.wav")
 
+
 enum AudioBus {
-	SOUNDTRACK,
+	BGM,
 	SFX,
 }
 
+
 const AUDIO_BUS_STRINGS: Dictionary[AudioBus, String] = {
-	AudioBus.SOUNDTRACK: "Soundtrack",
+	AudioBus.BGM: "BGM",
 	AudioBus.SFX: "SFX",
 }
 
+
 func _ready() -> void:
-	play_sound(SOUNDTRACK, AudioBus.SOUNDTRACK)
+	play_sound(SOUNDTRACK, AudioBus.BGM)
 
 
 func play_sound(stream: AudioStream, bus: AudioBus):
